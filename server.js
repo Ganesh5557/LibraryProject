1df6b5007461dc285cdda7099d4b47b54751f8fb
ganesh <ganesh.pandey5557@gmail.com>
2020-01-08 00:29:50 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const userRouter = require('./routes/index')
+const mongoose = require('mongoose')
+
+
+app.set('view engine', 'ejs')// Set the default view engine to ejs from js
+app.set ('views', __dirname + '/views');
+app.set('layout', 'layouts/layout')
+
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.use('/', userRouter)
+
+mongoose.connect(process.env.DATABASE_URL) //connection with the server
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'));
+
+
+app.listen(process.env.PORT || 3000)
